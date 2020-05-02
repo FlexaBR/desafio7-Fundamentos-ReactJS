f3c2239c62b66d24b0efd3b65f6bcb64c8b566cc
FlexaBR <r.gutheil@terra.com.br>
2020-05-02 18:00:37 -0300

    alterado p/ test Rocketseat (upload não windows)

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -1,5 +1,9 @@
 import React, { ReactNode } from 'react';
 
+// alterar se windows:
+// <Dropzone
+// accept="application/vnd.ms-excel"
+
 import Dropzone from 'react-dropzone';
 import { DropContainer, UploadMessage } from './styles';
 
@@ -27,10 +31,7 @@ const Upload: React.FC<UploadProps> = ({ onUpload }: UploadProps) => {
 
   return (
     <>
-      <Dropzone
-        accept="application/vnd.ms-excel"
-        onDropAccepted={(files) => onUpload(files)}
-      >
+      <Dropzone accept="text/csv" onDropAccepted={(files) => onUpload(files)}>
         {({ getRootProps, getInputProps, isDragActive, isDragReject }): any => (
           <DropContainer
             {...getRootProps()}
